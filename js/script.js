c4171d785178248486effa82790aca1d9f8596fc
Federico Verrengia <fedeverre.developer@gmail.com>
2020-11-05 18:41:21 +0100

    Feature added cart functionanilty (javascript)

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const cartProduct = document.querySelectorAll(".cart__product");
+const cartSummaryEl = document.querySelector(
+  ".cart__summary-total .cart__summary-price .value"
+);
+let cartSummary;
+
+for (let i = 0; i < cartProduct.length; i++) {
+  cartProduct[i]
+    .querySelector(".btn-quantity--minus")
+    .addEventListener("click", function () {
+      removeQuantity(i);
+    });
+  cartProduct[i]
+    .querySelector(".btn-quantity--plus")
+    .addEventListener("click", function () {
+      addQuantity(i);
+    });
+}
+
+const addQuantity = function (i) {
+  const cartQuantity = cartProduct[i].querySelector(".cart__quantity-input");
+  let quantity = Number(cartQuantity.value) + 1;
+  cartQuantity.value = quantity;
+  calcTotal();
+};
+
+const removeQuantity = function (i) {
+  const cartQuantity = cartProduct[i].querySelector(".cart__quantity-input");
+  let quantity = Number(cartQuantity.value) - 1;
+  if (quantity < 0) quantity = 0;
+  cartQuantity.value = quantity;
+  calcTotal();
+};
+
+const calcTotal = function () {
+  cartSummary = 0;
+  for (let i = 0; i < cartProduct.length; i++) {
+    const cartQuantity = Number(
+      cartProduct[i].querySelector(".cart__quantity-input").value
+    );
+    const cartPrice = Number(
+      cartProduct[i].querySelector(".cart__price--discounted .value")
+        .textContent
+    );
+    cartSummary = cartSummary + cartQuantity * cartPrice;
+    cartSummaryEl.textContent = cartSummary.toFixed(2);
+  }
+};
